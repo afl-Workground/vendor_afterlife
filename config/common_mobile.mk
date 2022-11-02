7fa50605298444e785186d9b6fcb7f68932f5ddd
Chris Crump <chriscrawford893@gmail.com>
2022-11-02 02:08:26 -0400

    overlay: Add face enroll overlays
    - Custom face enroll activity
    - Modified face enroll education lottie animation from Pixel
    - Face enroll configuration from Pixel
    
    Change-Id: I0daea264fa4364cafaa2f3165c29b3bf9b806209
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Co-authored-by: Sourajit Karmakar <sourajitk@statixos.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -6,6 +6,10 @@ PRODUCT_PACKAGES += \
     AvatarPicker \
     LatinIME
 
+# FaceEnroll - Settings RRO
+PRODUCT_PACKAGES += \
+    FaceEnrollSettingsOverlay
+
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
