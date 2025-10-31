33048dce89b2c8726551122a433842cbbc475d27
romeo <rbesar210311@gmail.com>
2025-10-31 22:06:06 +0000

    common: Build LMOFreeform
    Signed-off-by: romeo <rbesar210311@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -190,10 +190,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_land_l=64 \
     ro.com.google.ime.kb_pad_land_r=64
 
-#Omni 
+# Omni
 PRODUCT_PACKAGES += \
     OmniStyle \
     OmniJaws
+
+# LMOFreeform
+PRODUCT_PACKAGES += \
+    LMOFreeform \
+    LMOFreeformSidebar
 
 # Openssh
 PRODUCT_PACKAGES += \
